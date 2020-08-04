4ef5cab4b6a2a90e05b04bdcf37faf8b0df8b4f2
Bruno <brunnofn19@gmail.com>
2020-08-04 19:00:02 -0300

    add form and handle submit function

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import {
   Image,
   KeyboardAvoidingView,
@@ -6,6 +6,8 @@ import {
   Platform,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/mobile';
 
 import logoImg from '../../assets/logo.png';
 
@@ -20,7 +22,19 @@ import {
 } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
+
+  const handleSubmit = useCallback(
+    (data, { reset }) => {
+      console.log(data);
+
+      navigation.navigate('SignIn');
+
+      reset();
+    },
+    [navigation],
+  );
 
   return (
     <>
@@ -37,37 +51,44 @@ const SignUp: React.FC = () => {
 
             <ContentTitle>Faça seu cadastro</ContentTitle>
 
-            <Input
-              icon="user"
-              name="name"
-              placeholder="Digite seu nome"
-              autoCorrect={false}
-              autoCapitalize="words"
-            />
-            <Input
-              icon="mail"
-              name="email"
-              placeholder="Digite seu e-mail"
-              keyboardType="email-address"
-              autoCorrect={false}
-              autoCapitalize="none"
-            />
-            <Input
-              icon="smartphone"
-              name="phone"
-              placeholder="Digite seu telefone"
-              keyboardType="numeric"
-              autoCorrect={false}
-            />
-            <Input
-              icon="lock"
-              name="password"
-              placeholder="Digite sua senha"
-              returnKeyType="next"
-              secureTextEntry
-            />
+            <Form ref={formRef} onSubmit={handleSubmit}>
+              <Input
+                icon="user"
+                name="name"
+                placeholder="Digite seu nome"
+                autoCorrect={false}
+                autoCapitalize="words"
+              />
 
-            <Button>Cadastrar</Button>
+              <Input
+                icon="mail"
+                name="email"
+                placeholder="Digite seu e-mail"
+                keyboardType="email-address"
+                autoCorrect={false}
+                autoCapitalize="none"
+              />
+
+              <Input
+                icon="smartphone"
+                name="phone"
+                placeholder="Digite seu telefone"
+                keyboardType="numeric"
+                autoCorrect={false}
+              />
+
+              <Input
+                icon="lock"
+                name="password"
+                placeholder="Digite sua senha"
+                returnKeyType="next"
+                secureTextEntry
+              />
+            </Form>
+
+            <Button onPress={() => formRef.current?.submitForm()}>
+              Cadastrar
+            </Button>
           </Container>
         </ScrollView>
       </KeyboardAvoidingView>
