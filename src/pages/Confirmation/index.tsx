ea026b162d726de0f04886331b6f7edd45eef0e3
Bruno <brunnofn19@gmail.com>
2020-08-05 14:41:22 -0300

    add initial structure to confirmation page

--- src/pages/Confirmation/index.tsx
+++ src/pages/Confirmation/index.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import { api } from '../../services/api';
+import parkingIcon from '../../assets/parking-icon.png';
+
+import {
+  Container,
+  TopContent,
+  ParkingName,
+  ParkingPhone,
+  ParkingAddress,
+  ParkingPrice,
+} from './styles';
 
 interface IConfirmationProps {
   route: {
@@ -10,12 +21,47 @@ interface IConfirmationProps {
   };
 }
 
+interface IParkingLotState {
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  five_minuts_price: number;
+}
+
 const Confirmation: React.FC<IConfirmationProps> = ({ route }) => {
-  console.log(route.params.parking_id);
+  const [parkingLot, setParkingLot] = useState<IParkingLotState>(
+    {} as IParkingLotState,
+  );
 
-  // fazer chamada api para pegar informações do estacionamento
+  useEffect(() => {
+    async function loadParkingLotInformations(): Promise<void> {
+      const response = await api.get(
+        `/parking/${route.params.parking_id}/info`,
+      );
 
-  return <Container />;
+      setParkingLot(response.data);
+    }
+
+    loadParkingLotInformations();
+  }, [route.params.parking_id]);
+
+  return (
+    <Container>
+      {parkingLot && (
+        <TopContent>
+          <Image source={parkingIcon} />
+
+          <ParkingName>{parkingLot.name}</ParkingName>
+          <ParkingAddress>{parkingLot.address}</ParkingAddress>
+          <ParkingPhone>{parkingLot.phone}</ParkingPhone>
+          <ParkingPrice>
+            R$ {parkingLot.five_minuts_price} / 5 minutos
+          </ParkingPrice>
+        </TopContent>
+      )}
+    </Container>
+  );
 };
 
 export { Confirmation };
--- src/pages/Confirmation/styles.ts
+++ src/pages/Confirmation/styles.ts
@@ -3,4 +3,41 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background: #1f1f1f;
+  align-items: center;
+  padding: 0 30px;
+`;
+
+export const TopContent = styled.View`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ParkingName = styled.Text`
+  font-family: 'Roboto-Medium';
+  font-size: 20px;
+  color: #fff;
+  margin-top: 20px;
+  text-align: center;
+`;
+
+export const ParkingAddress = styled.Text`
+  font-family: 'Roboto-Regular';
+  font-size: 15px;
+  color: #7d7d7d;
+  text-align: center;
+`;
+
+export const ParkingPhone = styled.Text`
+  font-family: 'Roboto-Regular';
+  font-size: 15px;
+  color: #7d7d7d;
+  margin-bottom: 20px;
+  text-align: center;
+`;
+
+export const ParkingPrice = styled.Text`
+  font-family: 'Roboto-Medium';
+  font-size: 20px;
+  color: #29c872;
 `;
