72738f1bd65cc16821c6a4340cd88d84071f702a
Bruno <brunnofn19@gmail.com>
2020-08-04 22:14:25 -0300

    add marker style

--- src/pages/Scanner/index.tsx
+++ src/pages/Scanner/index.tsx
@@ -4,13 +4,14 @@ import { useNavigation } from '@react-navigation/native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 
 import Button from '../../components/Button';
-
 import logoImg from '../../assets/logo.png';
 
 const Scanner: React.FC = () => {
   const [qrCodeData, setQrCodeData] = useState('');
 
   const navigation = useNavigation();
+
+  console.log(qrCodeData);
 
   const onSuccess = useCallback(
     e => {
@@ -25,15 +26,17 @@ const Scanner: React.FC = () => {
     <QRCodeScanner
       onRead={e => onSuccess(e)}
       showMarker
+      markerStyle={{ borderRadius: 5, borderColor: '#29c872' }}
       checkAndroid6Permissions
       topContent={<Image source={logoImg} />}
       topViewStyle={{
-        paddingBottom: 40,
+        paddingBottom: 70,
       }}
+      // eslint-disable-next-line prettier/prettier
       bottomContent={(
         <Button
           style={{
-            height: '58%',
+            height: '53%',
             borderRadius: 0,
             backgroundColor: '#F55252',
             position: 'absolute',
@@ -43,6 +46,7 @@ const Scanner: React.FC = () => {
         >
           Cancelar
         </Button>
+        // eslint-disable-next-line prettier/prettier
       )}
       containerStyle={{ backgroundColor: '#1f1f1f' }}
     />
