a016f679f3f3492aed3953c081d25db82e1223f4
Bruno <brunnofn19@gmail.com>
2020-08-06 10:56:51 -0300

    add payment and vehicles initial structure

--- src/pages/Confirmation/index.tsx
+++ src/pages/Confirmation/index.tsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { Image } from 'react-native';
 
 import { api } from '../../services/api';
+
+import { SelectButton } from '../../components/SelectButton';
+import Button from '../../components/Button';
 import parkingIcon from '../../assets/parking-icon.png';
 
 import {
@@ -11,6 +14,14 @@ import {
   ParkingPhone,
   ParkingAddress,
   ParkingPrice,
+  PaymentContainer,
+  PaymentTitle,
+  VehiclesContainer,
+  VehiclesTitle,
+  VehiclesHorizontalScroll,
+  VehicleCard,
+  VehicleName,
+  VehicleLicensePlate,
 } from './styles';
 
 interface IConfirmationProps {
@@ -49,16 +60,62 @@ const Confirmation: React.FC<IConfirmationProps> = ({ route }) => {
   return (
     <Container>
       {parkingLot && (
-        <TopContent>
-          <Image source={parkingIcon} />
+        <>
+          <TopContent>
+            <Image source={parkingIcon} />
 
-          <ParkingName>{parkingLot.name}</ParkingName>
-          <ParkingAddress>{parkingLot.address}</ParkingAddress>
-          <ParkingPhone>{parkingLot.phone}</ParkingPhone>
-          <ParkingPrice>
-            R$ {parkingLot.five_minuts_price} / 5 minutos
-          </ParkingPrice>
-        </TopContent>
+            <ParkingName>{parkingLot.name}</ParkingName>
+            <ParkingAddress>{parkingLot.address}</ParkingAddress>
+            <ParkingPhone>{parkingLot.phone}</ParkingPhone>
+            <ParkingPrice>
+              R$ {parkingLot.five_minuts_price} / 5 minutos
+            </ParkingPrice>
+          </TopContent>
+
+          <PaymentContainer>
+            <PaymentTitle>Forma de pagamento</PaymentTitle>
+
+            <SelectButton iconName="credit-card">
+              Crédito ou Débito
+            </SelectButton>
+
+            <SelectButton iconName="dollar-sign">Dinheiro</SelectButton>
+          </PaymentContainer>
+
+          <VehiclesContainer>
+            <VehiclesTitle>
+              Selecione o veículo que deseja estacionar
+            </VehiclesTitle>
+
+            <VehiclesHorizontalScroll
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{ padding: 10, paddingRight: 0 }}
+            >
+              <VehicleCard>
+                <VehicleName>Gol</VehicleName>
+                <VehicleLicensePlate>ABC0000</VehicleLicensePlate>
+              </VehicleCard>
+
+              <VehicleCard>
+                <VehicleName>Gol</VehicleName>
+                <VehicleLicensePlate>ABC0000</VehicleLicensePlate>
+              </VehicleCard>
+
+              <VehicleCard>
+                <VehicleName>Gol</VehicleName>
+                <VehicleLicensePlate>ABC0000</VehicleLicensePlate>
+              </VehicleCard>
+
+              <VehicleCard>
+                <VehicleName>Gol</VehicleName>
+                <VehicleLicensePlate>ABC0000</VehicleLicensePlate>
+              </VehicleCard>
+            </VehiclesHorizontalScroll>
+          </VehiclesContainer>
+
+          <Button onPress={() => console.log('Estacionar')}>Estacionar</Button>
+        </>
       )}
     </Container>
   );
--- src/pages/Confirmation/styles.ts
+++ src/pages/Confirmation/styles.ts
@@ -17,7 +17,7 @@ export const ParkingName = styled.Text`
   font-family: 'Roboto-Medium';
   font-size: 20px;
   color: #fff;
-  margin-top: 20px;
+  margin-top: 6px;
   text-align: center;
 `;
 
@@ -32,12 +32,64 @@ export const ParkingPhone = styled.Text`
   font-family: 'Roboto-Regular';
   font-size: 15px;
   color: #7d7d7d;
-  margin-bottom: 20px;
+  margin-bottom: 6px;
   text-align: center;
 `;
 
 export const ParkingPrice = styled.Text`
   font-family: 'Roboto-Medium';
   font-size: 20px;
+  margin-bottom: 15px;
   color: #29c872;
+`;
+
+export const PaymentContainer = styled.View`
+  width: 100%;
+  height: 180px;
+  margin-bottom: 5px;
+`;
+
+export const PaymentTitle = styled.Text`
+  font-family: 'Roboto-Regular';
+  font-size: 15px;
+  color: #ffff;
+  margin-bottom: 6px;
+`;
+
+export const VehiclesContainer = styled.View`
+  width: 100%;
+  height: 170px;
+`;
+
+export const VehiclesTitle = styled.Text`
+  font-family: 'Roboto-Regular';
+  font-size: 15px;
+  color: #ffff;
+  margin-bottom: 6px;
+`;
+
+export const VehiclesHorizontalScroll = styled.ScrollView`
+  width: 100%;
+`;
+
+export const VehicleCard = styled.View`
+  width: 120px;
+  height: 120px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+  margin-right: 10px;
+  background: #29c872;
+`;
+
+export const VehicleName = styled.Text`
+  font-family: 'Roboto-Medium';
+  font-size: 15px;
+  color: #000;
+`;
+
+export const VehicleLicensePlate = styled.Text`
+  font-family: 'Roboto-Medium';
+  font-size: 15px;
+  color: #000;
 `;
