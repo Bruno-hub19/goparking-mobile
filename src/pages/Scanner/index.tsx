ee694b9228d019e702f90769f6eef66740eb8c5a
Bruno <brunnofn19@gmail.com>
2020-08-08 17:11:31 -0300

    add timeout to increase performance

--- src/pages/Scanner/index.tsx
+++ src/pages/Scanner/index.tsx
@@ -1,11 +1,19 @@
-import React, { useCallback } from 'react';
-import QRCodeScanner, { Event } from 'react-native-qrcode-scanner';
+import React, { useCallback, useState, useEffect } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import QRCodeScanner, { Event } from 'react-native-qrcode-scanner';
 
 import { ScannerTopContent } from '../../components/ScannerTopContent';
 
 const Scanner: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
   const navigation = useNavigation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(true);
+    }, 1000);
+  }, []);
 
   const onSuccess = useCallback(
     (e: Event) => {
@@ -16,22 +24,35 @@ const Scanner: React.FC = () => {
     [navigation],
   );
 
-  return (
-    <>
-      <QRCodeScanner
-        onRead={e => onSuccess(e)}
-        fadeIn
-        showMarker
-        cameraStyle={{ height: 430 }}
-        markerStyle={{ borderRadius: 5, borderColor: '#29c872' }}
-        checkAndroid6Permissions
-        topContent={<ScannerTopContent />}
-        topViewStyle={{
-          paddingBottom: 80,
+  if (!isVisible) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#1f1f1f',
         }}
-        containerStyle={{ backgroundColor: '#1f1f1f' }}
-      />
-    </>
+      >
+        <ActivityIndicator size="large" color="#29c872" />
+      </View>
+    );
+  }
+
+  return (
+    <QRCodeScanner
+      onRead={e => onSuccess(e)}
+      fadeIn
+      showMarker
+      cameraStyle={{ height: 430 }}
+      markerStyle={{ borderRadius: 5, borderColor: '#29c872' }}
+      checkAndroid6Permissions
+      topContent={<ScannerTopContent />}
+      topViewStyle={{
+        paddingBottom: 80,
+      }}
+      containerStyle={{ backgroundColor: '#1f1f1f' }}
+    />
   );
 };
 
