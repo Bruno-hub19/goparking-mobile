8cbca24dc9286abbf64ff9c82a98936cf4db7508
Bruno <brunnofn19@gmail.com>
2020-08-08 11:01:50 -0300

    remove token

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,13 +1,7 @@
 import axios from 'axios';
-import AsyncStorage from '@react-native-community/async-storage';
-
-const token = AsyncStorage.getItem('@GoParking:token').then(
-  response => response,
-);
 
 const api = axios.create({
   baseURL: 'http://localhost:3434',
-  headers: { Authorization: token },
 });
 
 export { api };
