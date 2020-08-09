167b1b8308c84fab3c1877fef7ba8d26bc605d3f
Bruno <brunnofn19@gmail.com>
2020-08-09 07:41:09 -0300

    remove title align

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -64,7 +64,6 @@ const VehiclesStackScreens: React.FC = ({ navigation }: any) => {
     <VehiclesStack.Navigator
       screenOptions={{
         title: '',
-        headerTitleAlign: 'center',
         headerTintColor: '#29c872',
         headerStyle: {
           backgroundColor: '#1f1f1f',
