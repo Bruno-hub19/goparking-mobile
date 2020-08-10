b170656083fb81bd9a4276385ebb44e8a2b64095
Bruno <brunnofn19@gmail.com>
2020-08-10 12:31:06 -0300

    add new style

--- src/components/ScannerTopContent/styles.ts
+++ src/components/ScannerTopContent/styles.ts
@@ -3,7 +3,8 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   width: 100%;
   height: 100%;
-  justify-content: space-between;
+  padding-top: 20px;
+  justify-content: center;
   align-items: center;
 `;
 
