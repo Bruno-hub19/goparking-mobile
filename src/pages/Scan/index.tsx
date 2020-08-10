bca83f26b1494af2ec436c62a711ff461030f60a
Bruno <brunnofn19@gmail.com>
2020-08-10 18:38:29 -0300

    remove button

--- src/pages/Scan/index.tsx
+++ src/pages/Scan/index.tsx
@@ -1,19 +1,15 @@
 import React, { useEffect } from 'react';
 import { Image } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 
 import { useAuth } from '../../hooks/auth';
 import { useVehicle } from '../../hooks/vehicle';
 
-import Button from '../../components/Button';
 import logoImg from '../../assets/logo.png';
 import qrcodeImage from '../../assets/qr-code.png';
 
 import { Container, HelloText, InstructionText } from './styles';
 
 const Scan: React.FC = () => {
-  const navigation = useNavigation();
-
   const { user, token } = useAuth();
   const { loadVehicles } = useVehicle();
 
@@ -34,10 +30,6 @@ const Scan: React.FC = () => {
       </InstructionText>
 
       <Image source={qrcodeImage} style={{ marginBottom: 60 }} />
-
-      <Button onPress={() => navigation.navigate('Scanner')}>
-        Abrir scanner
-      </Button>
     </Container>
   );
 };
