8b0fa3a4ffaa5504403511ba8440683508491389
Bruno <brunnofn19@gmail.com>
2020-08-11 17:34:58 -0300

    add padding bottom

--- src/components/ScannerTopContent/styles.ts
+++ src/components/ScannerTopContent/styles.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   width: 100%;
   height: 100%;
-  padding-top: 20px;
   justify-content: center;
   align-items: center;
 `;
